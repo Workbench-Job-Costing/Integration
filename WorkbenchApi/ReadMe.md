177281e9fa32ad1ee706c46e3bd18d8116253be4
Anton Greb <antong@wbi.co.nz>
2019-05-16 09:24:58 +1200

    publish workbench API C# client

---
+++ WorkbenchApi/ReadMe.md
@@ -0,0 +1,8 @@
+# Workbench API Integration
+
+Generate your stub methods using the [Birch Swagger](https://www.nuget.org/packages/Birch.Swagger.ProxyGenerator) package 
+
+Retrieve a list of companies by using the filter criteria 
+
+Requires Visual Studio 2017 and .NET Framework 4.5
+ 
---
+++ WorkbenchApi/WorkbenchApiClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkbenchApiClient.Swagger;
+
+namespace WorkbenchApiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () => await MainAsync()).Wait();
+
+            Console.WriteLine("Press any key to continue...");
+
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync()
+        {
+            var client = new CompanyListApiWebProxy(new Uri("https://web.workbench.co.nz/WorkbenchV4/"));
+            var result = await client.Post(new GridRequestParametersApi
+            {
+                predicate = new DynamicPredicateApi
+                {
+                    PredicateRows = new List<DynamicPredicateRowApi>
+                    {
+                        new DynamicPredicateRowApi
+                        {
+                            Display = true,
+                            LeftOperand = "CompanyName",
+                            Operator = DynamicPredicateRowApi.OperatorValues.Like,
+                            RightOperand = new List<string>{ "Work" } 
+                        }
+                    }
+                },
+                page = 1,
+                rows = 20,
+                sidx = "CompanyName",
+                sord = "asc"
+            });
+            Console.WriteLine("Total Records:" + result.records);
+        }
+    }
+}
